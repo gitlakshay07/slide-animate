ec085448d02d673ccbe7cc34948a65fd892fa766
gitlakshay10 <lakshay@lancepaas.com>
2024-09-25 10:16:35 +0530

    last commit

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.to(".img-container",{
+    width:"80%",
+    ease:Expo.easeInOut,
+    stagger:3
+})
+
+gsap.to(".text-container h1",{
+    top:"0",
+    ease:Expo.easeInOut,
+    stagger:3
+})
+gsap.to(".text-container h1",{
+    top:"0",
+    delay:3,
+    ease:Expo.easeInOut,
+    stagger:3,
+    top: "-120%"
+})
